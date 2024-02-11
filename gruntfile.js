9fff25b1300cd7b78a49fa41e3bf0a63b9199cf0
Lucas barros <fallenagamesytb@gmail.com>
2024-02-11 14:31:22 -0300

    f: corrigindo grunt

--- gruntfile.js
+++ gruntfile.js
@@ -54,11 +54,11 @@ module.exports = function(grunt) {
                     patterns: [
                         {
                             match: 'ENDERECO_DO_CSS',
-                            replacement: 'dist/styles/main.min.css'
+                            replacement: './styles/main.min.css'
                         },
                         {
                             match: 'ENDERECO_DO_JS',
-                            replacement: 'dist/scripts/main.min.js'
+                            replacement: './scripts/main.min.js'
                         }
                         
                     ]
